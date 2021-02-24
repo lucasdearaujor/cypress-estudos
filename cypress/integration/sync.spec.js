2c237776e650a3f8f0ecfe0a116264e1e80d8c96
Lucas Araujo <lucasdearaujor@gmail.com>
2021-02-24 07:34:14 -0300

    Retrys e Find

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -16,4 +16,27 @@ describe('Esperas...', () => {
         cy.get('#novoCampo').type('funcinou!!!')
 
     })
+
+    it.only('Deve fazer retrys', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+           // .should('not.exist')
+            .should('exist')
+            .type('Funcionou!!!')
+    })
+
+    it.only('Uso do find', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+        
+        //cy.get('#lista li')
+          //  .find('span')
+            //.should('contain', 'Item 2')        
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+
+
+    })
 })
