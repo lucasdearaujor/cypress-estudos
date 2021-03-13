7f989b5b9fb409c78764f9310f996240c0fc7516
Lucas Araujo <lucasdearaujor@gmail.com>
2021-03-13 08:57:40 -0300

    Desafio validar mensagens

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -61,4 +61,28 @@ describe('Work with alerts', () => {
         })
         cy.get('#prompt').click()
     })    
+
+    it.only('Validando mensagens', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+        
+        cy.get('#formNome').type('Lucas')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Araujo Ribeiro')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+        
+        cy.get('#resultado > :nth-child(1)')
+        .should('contain','Cadastrado')
+
+    })
+
+
 })
